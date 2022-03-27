a22067f2b25a797cca32ce7ac38e76bc54f3caab
Joel Kleibs <joel.kleibs@student.nhlstenden.com>
2022-03-27 15:17:45 +0200

    preBranch commit

--- src/EnergyPackage.java
+++ src/EnergyPackage.java
@@ -1,10 +1,7 @@
-import com.Decorator.EnergyFactory;
-
 public class EnergyPackage {
     private Double energyUnits;
     private Double heatUnits;
     private Double steamUnits;
-    private EnergyFactory eng;
 
     public EnergyPackage(Double energyUnits, Double heatUnits, Double steamUnits) {
         this.energyUnits = energyUnits;
--- src/com/Materials/IonParticle.java
+++ src/com/Materials/IonParticle.java
@@ -2,7 +2,16 @@ package com.Materials;
 
 public class IonParticle extends Material {
 
+
     public IonParticle(Double energyPerUnit, Double heatPerUnit, Double steamPerUnit) {
-        super(energyPerUnit, heatPerUnit, steamPerUnit);
+        this.energyPerUnit = energyPerUnit;
+        this.heatPerUnit = heatPerUnit;
+        this.steamPerUnit = steamPerUnit;
+    }
+
+    public void test(){
+        Double testVar = 0.2;
+        System.out.println(testVar + this.energyPerUnit);
+
     }
 }
