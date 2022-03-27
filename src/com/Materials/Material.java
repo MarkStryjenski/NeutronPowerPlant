09ec3bbbc0208fa379ac2a61c90804566d2e8475
Joel Kleibs <joel.kleibs@student.nhlstenden.com>
2022-03-27 15:55:52 +0200

    base materials added to project

--- src/com/Materials/Material.java
+++ src/com/Materials/Material.java
@@ -5,4 +5,34 @@ public abstract class Material {
     protected Double heatPerUnit;
     protected Double steamPerUnit;
 
+    //getters setters
+    protected Double getEnergyPerUnit()
+    {
+        return energyPerUnit;
+    }
+
+    protected void setEnergyPerUnit(Double energyPerUnit)
+    {
+        this.energyPerUnit = energyPerUnit;
+    }
+
+    protected Double getHeatPerUnit()
+    {
+        return heatPerUnit;
+    }
+
+    protected void setHeatPerUnit(Double heatPerUnit)
+    {
+        this.heatPerUnit = heatPerUnit;
+    }
+
+    protected Double getSteamPerUnit()
+    {
+        return steamPerUnit;
+    }
+
+    protected void setSteamPerUnit(Double steamPerUnit)
+    {
+        this.steamPerUnit = steamPerUnit;
+    }
 }
---
+++ src/com/Materials/neutronParticle.java
@@ -0,0 +1,10 @@
+package com.Materials;
+
+public class neutronParticle extends Material {
+
+    public neutronParticle(Double energyPerUnit, Double heatPerUnit, Double steamPerUnit) {
+        this.energyPerUnit = 10.0;
+        this.heatPerUnit = 10.0;
+        this.steamPerUnit = 8.5;
+    }
+}
